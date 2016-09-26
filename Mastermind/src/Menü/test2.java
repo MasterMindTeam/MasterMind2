0a61949b148083cd6395bf949993cbaf8d9566b6
Sophia <sophia@abc.de>
2016-09-26 14:24:25 +0200

    Test

---
+++ Mastermind/src/Menü/test2.java
@@ -0,0 +1,13 @@
+package Menü;
+import java.awt.EventQueue;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+public class test2 {
+
+	public static void main (String [] args) throws IOException{
+		MainMenu f=new MainMenu();
+		
+	}
+}
